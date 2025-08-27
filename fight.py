96a3745b2a081f700a729680ec0cccca27b2634f
avndreas <andreas.vinod7474@gmail.com>
2025-08-27 16:39:23 -0400

    Working on collision detection

--- fight.py
+++ fight.py
@@ -3,12 +3,20 @@ from pygame.math import *
 from button import Button
 
 def run(screen, location, fighter):
+
+    # Enemy initialization
+    enemy = None
     with open("assets/characters.json", "r") as f:
         data = json.load(f)
-    enemy = data['characters'][fighter]
+    for item in data['characters']:
+        if item['name'] == fighter:
+            enemy = item
     print("Fighting ", enemy['name'])
+    if enemy == None:
+        print("I have no enemies...")
+        return
 
-    pygame.init()
+    # Screen and sprite setup
     screenInfo = pygame.display.Info()
     screenWidth = screenInfo.current_w
     screenHeight = screenInfo.current_h 
@@ -19,23 +27,46 @@ def run(screen, location, fighter):
     BG = pygame.transform.scale(BG, (screenWidth, screenHeight))
     # BG = pygame.transform.scale(BG, (screenWidth, screenHeight))
     KNIFE = pygame.image.load("assets/bread_knife.png")
+    ENEMY = pygame.image.load(enemy['image'])
 
 
+    # Clock setup
     running = True
     x = 0
     clock = pygame.time.Clock()
     delta_time = 0.1
 
-    knife_position = Vector2(screenWidth / 2 - KNIFE.get_width() / 2, screenHeight / 2 - KNIFE.get_height() / 2)
+    # Positioning stage
+    knife_dimensions = Vector2(KNIFE.get_width(), KNIFE.get_height())
+    knife_position = Vector2(screenWidth / 2 - knife_dimensions.x / 2, screenHeight / 2 - knife_dimensions.y / 2)
     knife_point = Vector2(KNIFE.get_width(), KNIFE.get_height()) + knife_position
 
+    enemy_dimensions = Vector2(ENEMY.get_width(), ENEMY.get_height())
+    enemy_position = Vector2((screenWidth / 2) - (enemy_dimensions.x / 2), (3 * screenHeight / 4) - (enemy_dimensions.y / 2))
+    print("Enemy position: ", enemy_position, 
+          "\nLeft corner: ", enemy_position.x - enemy_dimensions.x / 2)
+
+    # Physics setup
     velocity = 0
     max_velocity = 2000
     strength = 50
     stab_depth = 150
     stab_cooldown = False
 
-    # ----------- Setup above ------------
+    # Functions
+    def get_bounds(sprite, position): # This assumes the position is the center of the sprite, it's up to the rest of the code to ensure it's rendered that way
+        dimensions = Vector2(sprite.get_width(), sprite.get_height())
+        top_left = position - dimensions / 2
+        bottom_right = position + dimensions / 2
+        top_right = top_left + Vector2(dimensions.x, 0)
+        bottom_left = top_left + Vector2(0, dimensions.y)
+        return (top_left, top_right, bottom_left, bottom_right)
+
+    def render():
+        screen.blit(BG, (0, 0))
+        screen.blit(KNIFE, knife_position - knife_dimensions / 2)
+        screen.blit(ENEMY, enemy_position - enemy_dimensions / 2)
+        pygame.display.flip()
 
     def stab(knife_position, knife_point):
         stab_cooldown = True
@@ -44,27 +75,45 @@ def run(screen, location, fighter):
         while knife_position.y <= startpos + stab_depth:
             knife_position.y += 40/x
             x += 1
-            screen.blit(BG, (0, 0))
-            screen.blit(KNIFE, knife_position)
-            pygame.display.flip()
+            render()
+        knife_point.y = knife_position.y + KNIFE.get_height()
+        pygame.time.delay(50)
 
     def unstab(knife_position, knife_point):
+        knife_point.y = knife_position.y + KNIFE.get_height()
         startpos = knife_position.y
         x = 1
         while knife_position.y >= startpos - stab_depth:
             knife_position.y -= 40/x
             x += 1
-            screen.blit(BG, (0, 0))
-            screen.blit(KNIFE, knife_position)
-            pygame.display.flip()
+            render()
         stab_cooldown = False
 
     # Game loop
     while running:
-        screen.blit(BG, (0, 0))
-        screen.blit(KNIFE, knife_position)
+        render()
         pressed = pygame.key.get_pressed()
         #print(knife_point)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pass
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    location.cleared = True
+                    running = False
+                if event.key == pygame.K_SPACE and not stab_cooldown:
+                    stab(knife_position, knife_point)
+                    
+                    unstab(knife_position, knife_point)
+                    render()
+                    pass
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+                    sys.exit()
 
         if pressed[pygame.K_d] and not pressed[pygame.K_a]:
             if velocity < max_velocity:
@@ -83,37 +132,22 @@ def run(screen, location, fighter):
         knife_position.x += velocity * delta_time
         knife_point = Vector2(KNIFE.get_width(), KNIFE.get_height()) + knife_position
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-                sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                pass
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RETURN:
-                    location.cleared = True
-                    running = False
-                if event.key == pygame.K_SPACE and not stab_cooldown:
-                    stab(knife_position, knife_point)
-                    screen.blit(BG, (0, 0))
-                    screen.blit(KNIFE, knife_position)
-                    pygame.display.flip()
-                    unstab(knife_position, knife_point)
-                if event.key == pygame.K_ESCAPE:
-                    running = False
-                    sys.exit()
+        # Collisions
+        if knife_position.x - knife_dimensions.x / 2 < 0:
+            knife_position.x = knife_dimensions.x / 2
+            velocity = -velocity
+        if knife_position.x + knife_dimensions.x / 2 > screenWidth:
+            knife_position.x = screenWidth - knife_dimensions.x / 2
+            velocity = -velocity
+        if knife_position.x > enemy_position.x - enemy_dimensions.x / 2:
+            if get_bounds(KNIFE, knife_position)[1].x >= get_bounds(ENEMY, enemy_position)[0].x:
+                print("Boom")
 
-        # Physics
-        if knife_position.x < 0:
-            knife_position.x = 0
-            velocity = -velocity
-        if knife_point.x > screenWidth:
-            knife_position.x = screenWidth - KNIFE.get_width()
-            velocity = -velocity
 
         # --------------- Maintenance stuff ------------------
         
         pygame.display.flip()
         delta_time = clock.tick(60) / 1000
         delta_time = max(0.001, min(0.1, delta_time))
+
 
--- placeview.py
+++ placeview.py
@@ -5,8 +5,14 @@ import pygame, fight, mapview, json
 def run(screen, location):
     with open("assets/characters.json", "r") as f:
         data = json.load(f)
-    fight.run(screen, location, data['locations'][location.name]['fighter'])
-    
+    if not location.cleared:
+        for item in data['locations']:
+            if item["name"] == location.name and "fighter" in item:
+                print("Starting fight with", item["fighter"])
+                fight.run(screen, location, item["fighter"])
+    print("Cleared ", location.name)
+    location.cleared = True
+
 
 
 class Place():
